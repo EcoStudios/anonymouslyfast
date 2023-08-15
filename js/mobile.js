504ad5427a5a6e2532cdf43761d9ec03e4316d1c
fastskating12 <calebshearer1@gmail.com>
2023-08-15 00:17:56 -0400

    FFixed some stuff with mobile pages

--- js/mobile.js
+++ js/mobile.js
@@ -3,9 +3,14 @@
 function menuClick(){
 	if (document.getElementById("mobile_menu").innerHTML == "menu") {
 		document.getElementById("mobile_navbar").style.visibility = "visible";
+		document.getElementById("mobile_navbar").style.height = "auto";
+		document.getElementById("mobile_navbar").style.width = "auto";
 		document.getElementById("mobile_menu").innerHTML = "menu_open";
+
 	} else {
 		document.getElementById("mobile_navbar").style.visibility = "hidden";
+		document.getElementById("mobile_navbar").style.height = "0px";
+		document.getElementById("mobile_navbar").style.width = "0px";
 		document.getElementById("mobile_menu").innerHTML = "menu";
 	}
 }
